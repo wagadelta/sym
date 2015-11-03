7caef945e547ac3f02038c35134f5dfb529d1ee8
wagagt-vaio <wagagt@gmail.com>
2015-11-03 08:15:15 -0600

    change permissions, and add faker to personas

--- database/seeds/PersonasTableSeeder.php
+++ database/seeds/PersonasTableSeeder.php
@@ -1,20 +1,31 @@
 <?php
- 
+
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Faker\Factory as Faker;
 
 
 class PersonasTableSeeder extends Seeder {
+
 public function run()
  {
-    $faker = Faker\Factory::create('es_ES');
-    $persona = 
-    [
-     'nombre' => $faker->name, 
-     'identificacion' => $faker->randomNumber($nbDigits = NULL)
-    ];
-    dd($persona);
-    //DB::table('personas')->insert($rol);
- }
- 
-}
+    $faker = Faker::create('es_ES');
+
+    foreach(range(1,30) as $index)
+    {
+        \Personas::create([
+          'nombre' => $faker->name ,
+          'identificacion' => $faker->randomNumber(11) ,
+          'otra_identificacion' =>  $faker->randomNumber(6) ,
+          'fecha_nacimiento' =>  $faker->dateTimeBetween($startDate = '-50 years', $endDate = 'now') ,
+          'domicilio' =>  $faker->address,
+          'telefonos' =>  $faker->randomNumber(8),
+          'foto' =>  'path',
+          'foto_dpi' =>  'path',
+          'conyugue_nombre' =>  $faker->name,
+          'conyugue_lugar_trabajo' => $faker->address ,
+          'estado' => $faker->randomElement($array = array ('activo','cancelado','suspendido'))
+        ]);
+    } //foreach
+ }//function
+}//class
