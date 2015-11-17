ef90cfa2ce752bf4d6ea9e1f7a9edb2e20de99e4
wagagt <wilvergonzalez.delta@gmail.com>
2015-11-17 11:12:58 +0000

    Save images from LoremImages to storate/images

--- database/seeds/ImagenesTableSeeder.php
+++ database/seeds/ImagenesTableSeeder.php
@@ -10,6 +10,7 @@ class ImagenesTableSeeder extends Seeder {
 public function run()
  {
     $faker = Faker::create('es_ES');
+    $imagePath = '/storage/images/';
     
     foreach(range(1,25) as $index)
      {
@@ -21,17 +22,27 @@ public function run()
           $etiquetas .=  $faker->numberBetween($min = 1, $max = 500)."|";
          }
 
+     $fotografoId = $faker->randomElement($array = array (3,5,6));
+     $ubicacionId = $faker->numberBetween($min = 1, $max = 200);
+     
+     
+    // thumbnail
+      $imageFake     = file_get_contents($faker->imageUrl('150', '100', 'sports'));
+      $imageName     = 'f-'.$fotografoId.'_u-'.$ubicacionId.'-thumb.jpg';
+      $thumb_to_save = base_path() .$imagePath. $imageName;
+      file_put_contents($thumb_to_save, $imageFake);
+    
       $imagen =
       [
-      'id_fotografo'      => $faker->randomElement($array = array (3,5,6)),
+      'id_fotografo'      => $fotografoId,
       'id_etiquetador'    => $faker->randomElement($array = array (2,4)),
-      'path'              => $faker->imageUrl(300,300, 'sports', 'Faker'),
-      'archivo'           => $faker->word.".jpg",
-      'slug'              => 'nombre-archivo',
-      'tipo_imagen'       => $faker->randomElement($array = array ('full','normal','thumb')),
+      'path'              => $thumb_to_save,
+      'archivo'           => $imageName,
+      'slug'              => $imageName,
+      'tipo_imagen'       => 'thumb', //$faker->randomElement($array = array ('full','normal','thumb')),
       'etiquetas'         => $etiquetas,
       'fecha_etiqueta'    => $faker->dateTimeThisYear($max='now'),
-      'id_ubicacion'      => $faker->numberBetween($min = 1, $max = 200),
+      'id_ubicacion'      => $ubicacionId,
       'estado'            => '1'
       ];
       DB::table('imagenes')->insert($imagen);
