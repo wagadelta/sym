b37d94477b19842ec456f1da413a8b512f772493
axelsarce <axelsarceno.delta@gmail.com>
2015-11-22 04:32:55 +0000

    Fix buscar por etiqueta corredor -|

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -56,19 +56,18 @@ class InicioController extends Controller {
 	
 	public static function searchByTag($corParam_1 = null, $runParam_2 = null, Request $request)
 	{
-		
+		DB::enableQueryLog();
 		$allImgsRun = DB::table('imagenes as i')
 			->join('ubicaciones as u', 'i.id_ubicacion', '=', 'u.id')
 			->select('i.id',  'i.archivo', 'i.id_ubicacion as ubicacion',
 						"i.etiquetas as tags", 'u.id_carrera as carrera')
-			->where('i.etiquetas', 'LIKE', '%|'.$runParam_2.'|%')
-			//->orWhere('i.etiquetas', 'LIKE', '%'.$runParam_2.'|%')
+			->where('i.etiquetas LIKE \'%|?|%\' OR i.etiquetas LIKE \'%?|%\' ', array($runParam_2,$runParam_2))
 			->where('i.tipo_imagen', '=', 'full')
 			->where('u.id_carrera', '=', $corParam_1)
 			->get();
 		$name = Imagenes::nameRunner($corParam_1);
 		$id = Imagenes::idRunner($corParam_1);
-		//dd($id);
+		dd(DB::getQueryLog());
 			
 		return  view('corredoresxcarrera')
 					->with('allImgsRun',$allImgsRun)
