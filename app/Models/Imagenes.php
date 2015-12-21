a0a233b14cc50e15a2ab184e2ae11bfd81d88eac
Axel Sarceño <axelsarceno.delta@gmail.com>
2015-12-21 08:43:06 -0600

    paginate show search amount images per page 25 to 24

--- app/Models/Imagenes.php
+++ app/Models/Imagenes.php
@@ -6,11 +6,11 @@ use App\Http\Requests;
 
 class Imagenes extends Model
 {
-    
+
 	public $table = "imagenes";
 
 	public $primaryKey = "id";
-    
+
 	public $timestamps = true;
 
 	public $fillable = [
@@ -23,7 +23,7 @@ class Imagenes extends Model
 		"etiquetas",
 		"fecha_etiqueta",
 		"id_ubicacion",
-		"estado", 
+		"estado",
 		"created_at",
 		"updated_at"
 	];
@@ -36,8 +36,8 @@ class Imagenes extends Model
 		"slug" => "required",
 		"tipo_imagen" => "required"
 	];
-	
-	
+
+
 	public static function ImgLastRunner()
 	{
 		return DB::table('carreras')
@@ -47,11 +47,11 @@ class Imagenes extends Model
 			->take(8)
 			->get();
 	}
-	
-	
+
+
 	public static function ImgSlideShow()
 	{
-		
+
 		return DB::table('imagenes')
 			->select('id', 'archivo')
 			->where('tipo_imagen', '=', 'full')
@@ -60,10 +60,10 @@ class Imagenes extends Model
 			->take(10)
 			->get();
 	}
-	
+
 	public static function ImgRunners($id)
 	{
-		
+
 		return DB::table('imagenes')
 		->select('id', 'nombre')
 		->where('id_carrera', '=', $id)
@@ -72,50 +72,50 @@ class Imagenes extends Model
 		->take(8)*/
 		->get();
 	}
-	
+
 	public static function ImgsRunners($id, $requests)
 	{
-		
+
 			// Devuelve todos las imagenes de esa carrera con paginacion
-		 
-		 
+
+
 		 $imagenes = DB::table('ubicaciones as u')
 			->join('imagenes as i', 'i.id_ubicacion', '=', 'u.id')
 			->select('i.id',  'i.archivo', 'i.id_ubicacion',"i.etiquetas as tags", 'u.id_carrera as carrera')
-			->where('i.tipo_imagen', '=', 'full')
+			->where('i.tipo_imagen', '=', 'normal')
 			->where('u.id_carrera', '=', $id)
-			->paginate(25); // cambiar parametro de pagineo al publicarlo de 15 imagenes por página
+			->paginate(24); // cambiar parametro de pagineo al publicarlo de 15 imagenes por página
 			$imagenes->setPath($requests->url());
 		return  $imagenes;
-		
-		
+
+
 	}
 	public static function nameRunner($id)
 	{
-		
+
 		 $nameR = DB::table('carreras')->where('id', $id)->pluck('nombre');
-		 
-		return $nameR; 
-		
-		
+
+		return $nameR;
+
+
 	}
-	
+
 		public static function idRunner($id)
 	{
 		 $idRun = DB::table('carreras')->where('id', $id)->pluck('id');
-		 
-		return $idRun; 
-		
+
+		return $idRun;
+
 	}
-	
-	
+
+
 		public static function searchTagCorrId($idRace, $qry, $request)
 		{
 			$nombre = $qry;
-			
+
 				//dd($qry);
-				
-				
+
+
 				$allImgsRun = DB::table('imagenes as i')
 					->join('ubicaciones as u', 'i.id_ubicacion', '=', 'u.id')
 					->select('i.id',  'i.archivo', 'i.id_ubicacion as ubicacion',
@@ -124,24 +124,24 @@ class Imagenes extends Model
 					//->orWhere('i.etiquetas', 'LIKE', '%'.$runParam_2.'|%')
 					->where('i.tipo_imagen', '=', 'normal')
 					->where('u.id_carrera', '=', $idRace)
-					->paginate(25); // cambiar parametro de pagineo al publicarlo de 15 imagenes por página
-					//->get(); 
+					->paginate(24); // cambiar parametro de pagineo al publicarlo de 15 imagenes por página
+					//->get();
 					$allImgsRun->setPath($request->url());
 					return $allImgsRun;
-			
-			
-		
+
+
+
 		}
-		
-		
+
+
 		public static function searchTagCorrByName($idRace, $qry, $request)
 		{
 			//$nombre = $qry;
-				
+
 				DB::connection()->enableQueryLog(); // Inicia data base log
-				
-			//	dd($idCorre);  //imprime el script SQL de la consulta 
-				
+
+			//	dd($idCorre);  //imprime el script SQL de la consulta
+
 				$corredores = DB::table('corredores as c')
 					->select('c.id', 'c.nombres', 'c.apellidos')
 					->where('c.etiquetado', '=', '1')
@@ -150,18 +150,18 @@ class Imagenes extends Model
 					->orWhere('c.apellidos', 'LIKE', '%'.$qry.'%')
 					//->get(); // cambiar parametro de pagineo al publicarlo de 15 imagenes por página
 					//dd(DB::getQueryLog());
-					->paginate(25); // cambiar parametro de pagineo al publicarlo de 15 imagenes por página
-					 
-					
+					->paginate(24); // cambiar parametro de pagineo al publicarlo de 15 imagenes por página
+
+
 					//dd($corredores);
 					$corredores->setPath($request->url());
-					
+
 					return $corredores;
-				
-			
+
+
 		}
-	
-	
-	
+
+
+
 
 }
