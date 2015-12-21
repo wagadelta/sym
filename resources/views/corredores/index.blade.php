644b162852ab3f250acba545fa8415fcd9aa8b60
wagagt <wilvergonzalez.delta@gmail.com>
2015-12-21 03:28:58 +0000

    Add column 'Etiquetado' on Corredores index view.

--- resources/views/corredores/index.blade.php
+++ resources/views/corredores/index.blade.php
@@ -18,31 +18,33 @@
             {!! $corredores->render() !!}
                 <table class="table table-striped table-condensed">
                     <thead>
-                    <th>Nombres</th>
-			<th>Apellidos</th>
-			<th>Slug</th>
-			<th>BIB Number</th>
-			<th>Id Carrera</th>
-			<!--<th>Tiene imágenes</th>-->
-			<th>Estado</th>
-                    <th width="50px">Action</th>
+                        <th>Nombres</th>
+            			<th>Apellidos</th>
+            			<th>Slug</th>
+            			<th>BIB Number</th>
+            			<th>Id Carrera</th>
+            			<th>Estado</th>
+                        <th width="50px">Action</th>
+                        <th>Etiquetado</th>
+                    
                     </thead>
                     <tbody>
-
                     @foreach($corredores as $corredor)
-                        <tr>
-                            <td>{!! $corredor->nombres !!}</td>
-        					<td>{!! $corredor->apellidos !!}</td>
-        					<td>{!! $corredor->slug !!}</td>
-        					<td>{!! $corredor->bib_number !!}</td>
-        				 	<td>{!! $corredor->id_carrera !!}</td>
-        					<!--<td>{--!! $corredor->etiquetado !!--}</td>-->
-        					<td>{!! $corredor->estado !!}</td>
-                            <td>
-                                <a href="{!! route('corredores.edit', [$corredor->id]) !!}"><i class="glyphicon glyphicon-edit"></i></a>
-                                <a href="{!! route('corredores.delete', [$corredor->id]) !!}" onclick="return confirm('Are you sure wants to delete this Corredores?')"><i class="glyphicon glyphicon-remove"></i></a>
-                            </td>
-                        </tr>
+                    <tr>
+                        <td>{!! $corredor->nombres !!}</td>
+    					<td>{!! $corredor->apellidos !!}</td>
+    					<td>{!! $corredor->slug !!}</td>
+    					<td>{!! $corredor->bib_number !!}</td>
+    				 	<td>{!! $corredor->id_carrera !!}</td>
+    					<td>{!! $corredor->estado !!}</td>
+                        <td>
+                            <a href="{!! route('corredores.edit', [$corredor->id]) !!}"><i class="glyphicon glyphicon-edit"></i></a>
+                            <a href="{!! route('corredores.delete', [$corredor->id]) !!}" onclick="return confirm('Are you sure wants to delete this Corredores?')"><i class="glyphicon glyphicon-remove"></i></a>
+                        </td>
+                        <td>
+                            <?php echo ($corredor->etiquetado==1)? '<i class="fa fa-thumbs-o-up" style="color:green"></i>' : '<i class="fa fa-thumbs-o-down" style="color:red"></i>'; ?>
+                        </td>                        
+                    </tr>
                     @endforeach
                     </tbody>
                 </table>
