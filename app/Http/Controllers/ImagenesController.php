554b06abd1f85f23273a7c29335cecc090b63d6e
Axel Sarceño <axelsarceno.delta@gmail.com>
2015-12-21 08:22:32 -0600

    ImagenesController filter id

--- app/Http/Controllers/ImagenesController.php
+++ app/Http/Controllers/ImagenesController.php
@@ -29,46 +29,46 @@ class ImagenesController extends AppBaseController
 	}
 
 	/**
-	 * Display a listing of the Imagenes.
-	 *
-	 * @param Request $request
-	 *
-	 * @return Response
-	 */
+	* Display a listing of the Imagenes.
+	*
+	* @param Request $request
+	*
+	* @return Response
+	*/
 	public function index(Request $request)
 	{
-	    $input = $request->all();
+		$input = $request->all();
 
 		$imagenes = \DB::table('imagenes')
 		->where('tipo_imagen', '=', 'normal')
 		->orderBy('id_ubicacion', 'desc')
 		->paginate(50);
 		$imagenes->setPath($request->url());
-//dd($imagenes->render());
+		//dd($imagenes->render());
 		return view('imagenes.index')
-		    ->with('imagenes', $imagenes);
+		->with('imagenes', $imagenes);
 	}
 
 	/**
-	 * Show the form for creating a new Imagenes.
-	 *
-	 * @return Response
-	 */
+	* Show the form for creating a new Imagenes.
+	*
+	* @return Response
+	*/
 	public function create()
 	{
 		return view('imagenes.create');
 	}
 
 	/**
-	 * Store a newly created Imagenes in storage.
-	 *
-	 * @param CreateImagenesRequest $request
-	 *
-	 * @return Response
-	 */
+	* Store a newly created Imagenes in storage.
+	*
+	* @param CreateImagenesRequest $request
+	*
+	* @return Response
+	*/
 	public function store(CreateImagenesRequest $request)
 	{
-        $input = $request->all();
+		$input = $request->all();
 
 		$imagenes = $this->imagenesRepository->store($input);
 
@@ -78,12 +78,12 @@ class ImagenesController extends AppBaseController
 	}
 
 	/**
-	 * Display the specified Imagenes.
-	 *
-	 * @param  int $id
-	 *
-	 * @return Response
-	 */
+	* Display the specified Imagenes.
+	*
+	* @param  int $id
+	*
+	* @return Response
+	*/
 	public function show($id)
 	{
 		$imagenes = $this->imagenesRepository->findImagenesById($id);
@@ -98,11 +98,11 @@ class ImagenesController extends AppBaseController
 	}
 
 	/**
-	 * Show the form for editing the specified Imagenes.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
+	* Show the form for editing the specified Imagenes.
+	*
+	* @param  int  $id
+	* @return Response
+	*/
 	public function edit($id)
 	{
 		$imagenes = $this->imagenesRepository->findImagenesById($id);
@@ -117,13 +117,13 @@ class ImagenesController extends AppBaseController
 	}
 
 	/**
-	 * Update the specified Imagenes in storage.
-	 *
-	 * @param  int    $id
-	 * @param CreateImagenesRequest $request
-	 *
-	 * @return Response
-	 */
+	* Update the specified Imagenes in storage.
+	*
+	* @param  int    $id
+	* @param CreateImagenesRequest $request
+	*
+	* @return Response
+	*/
 	public function update($id, CreateImagenesRequest $request)
 	{
 		$imagenes = $this->imagenesRepository->findImagenesById($id);
@@ -142,12 +142,12 @@ class ImagenesController extends AppBaseController
 	}
 
 	/**
-	 * Remove the specified Imagenes from storage.
-	 *
-	 * @param  int $id
-	 *
-	 * @return Response
-	 */
+	* Remove the specified Imagenes from storage.
+	*
+	* @param  int $id
+	*
+	* @return Response
+	*/
 	public function destroy($id)
 	{
 		$imagenes = $this->imagenesRepository->findImagenesById($id);
@@ -164,7 +164,7 @@ class ImagenesController extends AppBaseController
 
 		return redirect(route('imagenes.index'));
 	}
-	
+
 	/* Method on route /image-upload. Resize, watermark, and insert in DB all images uploaded */
 	public function upload(Request $request)
 	{
@@ -172,17 +172,17 @@ class ImagenesController extends AppBaseController
 		ini_set('post_max_size', '64M');
 		ini_set('upload_max_filesize', '64M');
 		// GENERAL DATA
-		$fotografoId = $request->input('id_fotografo'); 
+		$fotografoId = $request->input('id_fotografo');
 		$ubicacionId = $request->input('ubicacion');
 		$now = Carbon::now()->toDateTimeString();
 		$fileTempName   = str_slug($now, '_');
 		$imagePath = '/public/uploads/';
 		$outputDir = base_path().$imagePath.'temp/';
- 		$img =  Image::make($_FILES['file']['tmp_name'][0]);
- 		$watermark = Image::make(base_path().'/public/images/sym-watermark.png');
+		$img =  Image::make($_FILES['file']['tmp_name'][0]);
+		$watermark = Image::make(base_path().'/public/images/sym-watermark.png');
 		$dataArray=array('id_fotografo' => $fotografoId, 'path' => $imagePath, 'slug' => '',
-			'archivo' => '', 'id_ubicacion' => $ubicacionId, 'estado' => '1');
-		
+		'archivo' => '', 'id_ubicacion' => $ubicacionId, 'estado' => '1');
+
 		// FULL image
 		$img->resize(1024, 768)->insert($watermark, 'bottom-right');
 		$imageName     = 'f-'.$fotografoId.'_u-'.$ubicacionId.'-'.$fileTempName.'-full.jpg';
@@ -190,7 +190,7 @@ class ImagenesController extends AppBaseController
 		$img->save($pathToSave);
 		$dataArray['archivo']=$imageName;
 		$this->addImageOnTable($dataArray, 'full');
-		
+
 		// NORMAL  image
 		$img = Image::make($pathToSave);
 		$imageName     = 'f-'.$fotografoId.'_u-'.$ubicacionId.'-'.$fileTempName.'-normal.jpg';
@@ -198,7 +198,7 @@ class ImagenesController extends AppBaseController
 		$img->resize(452, 340)->save($pathToSave);
 		$dataArray['archivo']=$imageName;
 		$this->addImageOnTable($dataArray, 'normal');
-		
+
 		// THUMB  image
 		$img = Image::make($pathToSave);
 		$imageName     = 'f-'.$fotografoId.'_u-'.$ubicacionId.'-'.$fileTempName.'-thumb.jpg';
@@ -208,38 +208,38 @@ class ImagenesController extends AppBaseController
 		$this->addImageOnTable($dataArray, 'thumb');
 
 	}
-	
+
 	private function addImageOnTable($data,$tipo_imagen){
 		$slugArchivo = str_slug($data['archivo'],'-');
 		$imagen =
-	      [
-	      'id_fotografo'      => $data['id_fotografo'],
-	      'id_etiquetador'    => '',
-	      'path'              => $data['path'],
-	      'archivo'           => $data['archivo'],
-	      'slug'              => $slugArchivo,
-	      'tipo_imagen'       => $tipo_imagen,
-	      'etiquetas'         => '',
-	      'id_ubicacion'      => $data['id_ubicacion'],
-	      'estado'            => $data['estado']
-	      ];
-	     DB::table('imagenes')->insert($imagen);
+		[
+			'id_fotografo'      => $data['id_fotografo'],
+			'id_etiquetador'    => '',
+			'path'              => $data['path'],
+			'archivo'           => $data['archivo'],
+			'slug'              => $slugArchivo,
+			'tipo_imagen'       => $tipo_imagen,
+			'etiquetas'         => '',
+			'id_ubicacion'      => $data['id_ubicacion'],
+			'estado'            => $data['estado']
+		];
+		DB::table('imagenes')->insert($imagen);
 	}
-	
+
 	public function getCarrera(Request $request)
 	{
-		$input = $request->all();		
+		$input = $request->all();
 		if (\Auth::user()->id_rol == 2 )
 		{  // if is Etiquetador ROL
-			// mostrar la siguiente imagen sin etiquetar o nueva		
-				$carreras = Carreras::get();
-				return view('etiquetador.show-carreras')
-						->with('carreras', $carreras);
+			// mostrar la siguiente imagen sin etiquetar o nueva
+			$carreras = Carreras::get();
+			return view('etiquetador.show-carreras')
+			->with('carreras', $carreras);
 
 		} // if auth
-		
+
 	}
-	
+
 	public function etiquetar($carreraId, Request $request)
 	{
 		$userId = \Auth::user()->id;
@@ -247,42 +247,42 @@ class ImagenesController extends AppBaseController
 		DB::enableQueryLog();
 		Cache::forget('imagenes');
 		$imagen = DB::table('imagenes')
-			->select('imagenes.id', 'imagenes.etiquetas')
-			->join('ubicaciones', 'imagenes.id_ubicacion', '=', 'ubicaciones.id')
-			->join('carreras', 'ubicaciones.id_carrera', '=', 'carreras.id')
-			->where ('carreras.id', '=', $carreraId)
-			->where ('imagenes.tipo_imagen', '=', 'normal')
-			->where ('imagenes.is_blocked', '=', '0')
-			->where('id_etiquetador', '=', 0)
-			->first();
-			//dd(array($imagen, DB::getQueryLog() ));
-			if(!$imagen) {	
-				return view('etiquetador.show-image-no-results');
-			}
-			//Imagenes::where-> first();
-	 
-	 	// blockImagenToEtiquetar() //BLOQUEAR IMAGEN
-	 	$imagenBlock = Imagenes::find($imagen->id); //select the book using primary id
-	 	$imagenBlock->is_blocked = '1';
-	 	$saved = $imagenBlock->save();
-	 	
-	 	if(!$saved){
-    		App::abort(500, 'Error NO GRABO');
+		->select('imagenes.id', 'imagenes.etiquetas')
+		->join('ubicaciones', 'imagenes.id_ubicacion', '=', 'ubicaciones.id')
+		->join('carreras', 'ubicaciones.id_carrera', '=', 'carreras.id')
+		->where ('carreras.id', '=', $carreraId)
+		->where ('imagenes.tipo_imagen', '=', 'normal')
+		->where ('imagenes.is_blocked', '=', '0')
+		->where('id_etiquetador', '=', 0)
+		->first();
+		//dd(array($imagen, DB::getQueryLog() ));
+		if(!$imagen) {
+			return view('etiquetador.show-image-no-results');
+		}
+		//Imagenes::where-> first();
+
+		// blockImagenToEtiquetar() //BLOQUEAR IMAGEN
+		$imagenBlock = Imagenes::find($imagen->id); //select the book using primary id
+		$imagenBlock->is_blocked = '1';
+		$saved = $imagenBlock->save();
+
+		if(!$saved){
+			App::abort(500, 'Error NO GRABO');
 		}else{
 			//dd(array($imagenBlock,$imagen->id,'saved ok',DB::getQueryLog()));
 		}
-	 	
-	 	$etiquetasArray  = array_filter(explode('|', $imagen->etiquetas) );
-	 	$etiquetasString = implode(", ", $etiquetasArray);
-	 	//dd(\Auth::user()->id);
-	 	
+
+		$etiquetasArray  = array_filter(explode('|', $imagen->etiquetas) );
+		$etiquetasString = implode(", ", $etiquetasArray);
+		//dd(\Auth::user()->id);
+
 		return view('etiquetador.show-image')
-				->with('image', $imagenBlock)
-				->with('etiquetasString', $etiquetasString)
-				->with('etiquetadorId', $userId)
-				->with('carreraId', $carreraId);
+		->with('image', $imagenBlock)
+		->with('etiquetasString', $etiquetasString)
+		->with('etiquetadorId', $userId)
+		->with('carreraId', $carreraId);
 	}
-	
+
 	public function updateEtiquetar($id, Request $request){
 		$input = $request->all();
 		// formatear comas -> pipe
@@ -297,8 +297,8 @@ class ImagenesController extends AppBaseController
 		$imagen->is_blocked = '0';
 
 		$saved = $imagen->save();
-			 	if(!$saved){
-    		App::abort(500, 'Error NO GRABO');
+		if(!$saved){
+			App::abort(500, 'Error NO GRABO');
 		}else{
 			// change string to array
 			$explode = explode(',', $etiquetas_coma);
@@ -310,38 +310,41 @@ class ImagenesController extends AppBaseController
 				$corredor->save();
 			}
 		}
-		
+
 		return redirect('/imagenes/etiquetar/carrera/'.$input['carreraId'].'?');
 		//return redirect('/imagenes/etiquetar');
-		
+
 	}
 
 	public function uploadImages(){
 		$carreras = Carreras::get();
 		//dd($carreras);
 		return view('fotografo.index')
-			->with('carreras', $carreras);
-		
+		->with('carreras', $carreras);
+
 	}
-	
+
 	public function searchById($id = null, Request $request1)
 	{
+		//DB::connection()->enableQueryLog();
 		$imagenes= Imagenes::where('etiquetas', 'LIKE', "%|$id|%")
-							->where ('tipo_imagen', 'full')
-							->get();
-							
+		->where('tipo_imagen', 'normal')
+		->where('etiquetas', '<>', '')
+		->where('etiquetas', '<>', '||')
+		->get();
+		//dd(DB::getQueryLog());
+
 		$corredor= Corredores::where('id', $id)
-							->first();							
+		->first();
 		////($c$corredor
 		//$NombreCorredor = $imagenes->corredor()->name; // TODO: DEBE DE SETEAR LOS RELATIONSHIPS.
 		return view('resultados.byname')
-		    ->with('images', $imagenes)
-		    ->with('corredorData', $corredor);
-		    //->with('corredorName', $NombreCorredor)   // TODO
+		->with('images', $imagenes)
+		->with('corredorData', $corredor);
+		//->with('corredorName', $NombreCorredor)   // TODO
 	}
 
-	
+
 
 
 }
-
